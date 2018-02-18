fe89ca3313bc23447a5c4c646e06a7d84dfe69d1
manish <manish@manishs-MacBook-Pro.local>
2018-02-18 23:15:28 +0530

    setup start script and hero ku port

--- webserver/serverwithhbs.js
+++ webserver/serverwithhbs.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const hbs = require("hbs");
+
+const port =process.env.PORT || 3000;
+
 var app = express();
+
+
 
 app.set("view engine","hbs");
 
@@ -19,12 +24,12 @@ app.use((req,res,next)=>{
 	console.log(req.url);
 	 next();
 });
-app.use((req,res,next)=>{
-	res.render("../webserver/views/about.hbs",{
-	 	Title:"Under Maintaince",
-	 	currentYear:new Date().getFullYear()
-	 });
-});
+// app.use((req,res,next)=>{
+// 	res.render("../webserver/views/about.hbs",{
+// 	 	Title:"Under Maintaince",
+// 	 	currentYear:new Date().getFullYear()
+// 	 });
+// });
 //middleware
 
 app.get("/about",(req,res)=>{
@@ -44,4 +49,6 @@ app.get("/",(req,res)=>{
 });
 
 
-app.listen(3000);
+app.listen(port,()=>{
+	console.log(`server is up on port ${port}`);
+});
